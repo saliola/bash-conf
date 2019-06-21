03371e29a9bf05b4cea9a7f4d32f9e9441fc4c7e
Franco Saliola <saliola@gmail.com>
2019-06-21 13:07:53 -0400

    macos: alias g to _my_git_cmd

--- scripts/git.bash
+++ scripts/git.bash
@@ -7,5 +7,13 @@ function _my_git_cmd() {
 }
 
 alias g='_my_git_cmd'
-source /usr/share/bash-completion/completions/git
+
+# source platform dependant configs
+UNAME=$(uname)
+if [[ $UNAME == 'Linux' ]]; then
+    source /usr/share/bash-completion/completions/git
+elif [[ $UNAME == 'Darwin' ]]; then
+    source $BASHCONF_DIR/scripts/git-completion.bash
+fi
+
 complete -o default -o nospace -F _git g
